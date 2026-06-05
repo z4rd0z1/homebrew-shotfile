1a51a66c000859e787fef642ae8a048d8bae3987
z4rd0z1 <z4rd0z123@gmail.com>
2026-06-05 10:54:35 +0200

    Update ShotFile to v1.6.2

--- Casks/shotfile.rb
+++ Casks/shotfile.rb
@@ -1,6 +1,6 @@
 cask "shotfile" do
-  version "1.6.1"
-  sha256 "686391f9686d29da17345704db02a49553440de7b01549e5a9c783be7dcbfa58"
+  version "1.6.2"
+  sha256 "d25044aa2ccd8833776357961f2cb7313d7a1fd5ef7e57cdadc71310f602b232"
 
   url "https://github.com/z4rd0z1/homebrew-shotfile/releases/download/v#{version}/ShotFile-#{version}-arm64-mac.zip"
 
